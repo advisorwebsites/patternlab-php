c1c26bb160e7b65f3252d91093569c4dc29a47dc
Lucas Rasmussen <lucasr@advisorwebsites.com>
2015-09-10 10:54:32 -0700

    [] Got colors and Javascript at least partially working.

---
+++ source/js/min/scripts-min.js
@@ -0,0 +1,1 @@
+$(document).foundation(),$(document).foundation("offcanvas","reflow"),$(".off-canvas-wrap").foundation("offcanvas","show","move-right");
---
+++ source/js/scripts.js
@@ -0,0 +1,3 @@
+$(document).foundation();
+$(document).foundation('offcanvas', 'reflow');
+$('.off-canvas-wrap').foundation('offcanvas', 'show', 'move-right');
